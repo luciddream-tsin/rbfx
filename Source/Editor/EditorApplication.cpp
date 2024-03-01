4c92ab66d643a731dd5286eaeeb4b472db9d1a54
luciddream-tsin <92364028+luciddream-tsin@users.noreply.github.com>
2024-03-01 18:15:14 +0800

    FIXME : 替换默认字体, 暂不生效.

--- Source/Editor/EditorApplication.cpp
+++ Source/Editor/EditorApplication.cpp
@@ -700,11 +700,14 @@ void EditorApplication::InitializeSystemUI()
     auto systemUI = GetSubsystem<SystemUI>();
     systemUI->ApplyStyleDefault(true, 1.0f);
 
-    ImFont* defaultFont = systemUI->AddFont("Fonts/NotoSans-Regular.ttf", notoSansRanges, 16.f);
+    ImGuiIO& io = ui::GetIO();
+
+    // ImFont* defaultFont = systemUI->AddFont("Fonts/NotoSans-Regular.ttf", notoSansRanges, 16.f);
+    // FIXME: Cannot find SimHei.ttf
+    ImFont* defaultFont = systemUI->AddFont("Fonts/SimHei.ttf", io.Fonts->GetGlyphRangesDefault(), 16.f);
     systemUI->AddFont("Fonts/" FONT_ICON_FILE_NAME_FAS, fontAwesomeIconRanges, 14.f, true);
     systemUI->AddFont("Fonts/" FONT_ICON_FILE_NAME_FAS, fontAwesomeIconRanges, 12.f, true);
 
-    ImGuiIO& io = ui::GetIO();
     io.FontDefault = defaultFont;
 
     ImFont* monoFont = systemUI->AddFont("Fonts/NotoMono-Regular.ttf", notoMonoRanges, 14.f);
